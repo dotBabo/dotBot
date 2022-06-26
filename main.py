e76484f93d37ae208f977522751d883cc46855f5
dotBabo <ebabarinde123@gmail.com>
2022-06-26 19:00:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ async def on_message(message):
         else:
             await message.channel.send('tails')
             return
-    if ("bot")  in message.content:
-        tomato= '🍅'
-        await message.add_reaction(tomato)
+    if ("bot") in message.content:
+        tomato="🍅"
+        await message.channel.add_reaction(tomato)
 
 
 
